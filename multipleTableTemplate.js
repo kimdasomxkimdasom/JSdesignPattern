604e84525becad297e0aaea6f32f0455b4d9a2d5
kimdasomxaend <kimxdaxsom@aend.co.kr>
2022-03-14 18:14:36 +0900

    template method 활용 예제

---
+++ multipleTableTemplate.js
@@ -0,0 +1,40 @@
+var datastore = {
+    process: function () {
+        this.start();
+        this.select();
+        this.end();
+        return true;
+    }
+};
+
+function inherit(proto) {
+    var F = function () { };
+    F.prototype = proto;
+    return new F();
+}
+
+function template() {
+    var multipleTable = inherit(datastore);
+
+    // implement template steps
+
+    multipleTable.start = function () {
+        console.log("구구단 : 시작");
+    };
+
+    multipleTable.select = function () {
+        console.log("구구단 : 진행 중");
+        for(let x=1; x<10; x++){
+                console.log(x+"단 은!")
+            for(let y=1; y<10; y++){
+                console.log(x+" x "+y+" = "+x*y);
+            }
+        }
+    };
+
+    multipleTable.end = function () {
+        console.log("구구단 : 끝");
+    };
+
+    multipleTable.process();
+}
